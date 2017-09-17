20f96be155b73a43884a62d1700edb8ce0c52d9f
Austen Schunk <aschunk4@gmail.com>
2017-09-17 14:00:45 -0400

    updating setup file

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ NAME = 'LibML'
 AUTHOR = 'Austen Schunk'
 EMAIL = 'aschunk4@gmail.com'
 DESCRIPTION = 'A collection of modules in python that can be used for machine learning'
-VERSION = '0.1'
-PACKAGES = ['Classification', 'Utilities']
+VERSION = '0.1.03'
+PACKAGES = ['libml', 'libml.Classification', 'libml.Utilities']
 LICENSE = 'MIT License'
+URL = 'https://pypi.python.org/packages/c3/10/3a58547058f5197bc19a9d252b9a68368d6707d52dac058f263c7201c294/LibML-0.1.03.tar.gz'
 
 setup(
 	name = NAME,
@@ -16,4 +17,5 @@ setup(
 	description = DESCRIPTION,
 	packages = PACKAGES,
 	license = LICENSE,
+	url = URL,
 	)
